ae5aead28fbf64b0ebe3da962daee38164935bc3
Erik McClure <erikm@fundament.software>
2024-04-15 23:13:40 -0700

    Make traits async

--- src/logsource.rs
+++ src/logsource.rs
@@ -1,14 +1,19 @@
+use std::cell::RefCell;
+
 use super::caplog::CapLog;
 use super::log_capnp::log_source;
 use capnp_macros::capnproto_rpc;
 
 #[capnproto_rpc(log_source)]
-impl<const BUFFER_SIZE: usize> log_source::Server for CapLog<BUFFER_SIZE> {
-    fn get(&mut self, snowflake_id: u64, machine_id: u64, schema: u64, verify: bool) {
+impl<const BUFFER_SIZE: usize> log_source::Server for RefCell<CapLog<BUFFER_SIZE>> {
+    async fn get(&self, snowflake_id: u64, machine_id: u64, schema: u64, verify: bool) {
         let _ = schema;
 
-        match self.get_log(snowflake_id, machine_id, verify, &mut results.get().init_payload()) {
-            Ok(_) => capnp::ok(),
+        match self
+            .borrow_mut()
+            .get_log(snowflake_id, machine_id, verify, &mut results.get().init_payload())
+        {
+            Ok(_) => Ok(()),
             Err(e) => Err(capnp::Error::failed(e.to_string())),
         }
     }
